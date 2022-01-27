aa317eaecb3d6ec4011e8f8c2650afaa32ca64cc
Mai-Quang-Linh <93817168+Mai-Quang-Linh@users.noreply.github.com>
2022-01-27 22:18:15 +0900

    change out for vue

--- dist/js/portfolio.js
+++ dist/js/portfolio.js
@@ -29,3 +29,71 @@ function hideIconTitle(id) {
     $("#" + id).addClass("hide");
     $("#" + id).removeClass("show");
 }
+
+Vue.component('navlink-item', {
+    props: ['data'],
+    template: ` <div class="a_holder">
+                    <a :href="data.link">{{data.text}}</a>
+                </div>`
+});
+
+Vue.component('contact-icon', {
+    props: ['data'],
+    template: ` <li v-on:mouseover="data.mouseOver" v-on:mouseleave="data.mouseLeave">
+                    <img :src="data.imgSrc" class="contact icon">
+                </li>`
+});
+
+Vue.component('contact-icon-title', {
+    props: ['data'],
+    template: ` li id="icon_title{{data.id}}">
+                    <span>{{data.text}}</span>
+                </li>`
+});
+
+
+var navlinks = new Vue({
+    el: "#nav_links",
+    data: {
+        items: [
+            { id: 0, text: "me", link: "" },
+            { id: 1, text: "my works", link: "./project.html" },
+            { id: 2, text: "why me", link: "./cv.html" }
+        ]
+    }
+});
+
+var contactbox = new Vue({
+    el: '#contact_box',
+    data: {
+        items: [{
+                id: 0,
+                imgSrc: "./img/mail_icon.png",
+                text: "mai.quang.linh.hl97@gmail.com",
+                mouseOver: function() { showIconTitle('icon_title1') },
+                mouseLeave: function() { hideIconTitle('icon_title1') }
+            },
+            {
+                id: 1,
+                imgSrc: "./img/linkedin_icon.png",
+                text: "linkedin",
+                mouseOver: function() { showIconTitle('icon_title2') },
+                mouseLeave: function() { hideIconTitle('icon_title2') }
+            },
+            {
+                id: 2,
+                imgSrc: "./img/facebook_icon.png",
+                text: "facebook",
+                mouseOver: function() { showIconTitle('icon_title3') },
+                mouseLeave: function() { hideIconTitle('icon_title3') }
+            },
+            {
+                id: 3,
+                imgSrc: "./img/github_icon.png",
+                text: "github",
+                mouseOver: function() { showIconTitle('icon_title4') },
+                mouseLeave: function() { hideIconTitle('icon_title4') }
+            },
+        ]
+    }
+})
